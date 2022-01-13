bd04dc854463255e68e4ab50bd027475fb489e41
Xander Vertegaal <xander.vertegaal@xs4all.nl>
2022-01-13 12:01:47 +0100

    Flask app functions

---
+++ test_main.py
@@ -0,0 +1,18 @@
+import pytest
+from main import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_returns_200(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_foo_yields_bar(client):
+    response = client.get('/foo')
+    assert response.data == b'bar'
